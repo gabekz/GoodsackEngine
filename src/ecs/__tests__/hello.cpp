b969aeb94afece9883206bd4ad7c9405cafb4277
GabeKutuzov <gabe@kutuzov.dev>
2022-10-27 17:50:43 -0400

    [gtest] initialization

---
+++ src/ecs/__tests__/hello.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include <ecs/ecs.h>
+
+TEST(HELLOGROUP, TestName) {
+    EXPECT_EQ(1, 1);
+}
---
+++ testgtest.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(TestGroup, TestName) {
+    EXPECT_EQ(1, 1);
+}
