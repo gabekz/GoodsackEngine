e3dddf298d1bb5c25df9540f9f237b9c5d18b72a
gabekz <gabe@kutuzov.dev>
2023-12-12 22:58:20 -0500

    [physics] moved collision detection-checks to a list, allowing multiple colliding objects

--- goodsack/gsk/gsk/entity/modules/physics/collider_setup-system.c
+++ goodsack/gsk/gsk/entity/modules/physics/collider_setup-system.c
@@ -14,6 +14,8 @@
 #include "physics/physics_collision.h"
 #include "physics/physics_solver.h"
 
+#define MAX_COLLISION_POINTS 32
+
 static void
 init(gsk_Entity e)
 {
@@ -109,6 +111,9 @@ fixed_update(gsk_Entity e)
 
     collider->isColliding = 0;
 
+    gsk_CollisionPoints points_list[MAX_COLLISION_POINTS];
+    u32 points_list_next = 0;
+
     for (int i = 0; i < e.ecs->nextIndex; i++) {
         if (e.index == (gsk_EntityId)i) continue; // do not check self
 
@@ -128,64 +133,70 @@ fixed_update(gsk_Entity e)
         struct ComponentTransform *compareTransform =
           gsk_ecs_get(e_compare, C_TRANSFORM);
 
-        gsk_CollisionPoints points = {.has_collision = 0};
+//-----------------------
+// parameters used for all collision-check functions
+//
+// NOTE: this may be a little hacky, but it makes the collision-comparison
+// a lot more readable.
+#define __CLSN_PARAMS__                                            \
+    ((gsk_Collider *)collider->pCollider)->collider_data,          \
+      ((gsk_Collider *)compareCollider->pCollider)->collider_data, \
+      transform->position, compareTransform->position
 
         //
         // determine which collision-test function to use
         //
-        // sphere v. plane
-        if (collider->type == COLLIDER_SPHERE &&
-            compareCollider->type == COLLIDER_PLANE) {
-            points = gsk_physics_collision_find_sphere_plane(
-              ((gsk_Collider *)collider->pCollider)->collider_data,
-              ((gsk_Collider *)compareCollider->pCollider)->collider_data,
-              transform->position,
-              compareTransform->position);
-        }
+        gsk_CollisionPoints points = {.has_collision = 0};
 
-        // plane v. sphere
-        else if (collider->type == COLLIDER_PLANE &&
-                 compareCollider->type == COLLIDER_SPHERE) {
-            points = gsk_physics_collision_find_plane_sphere(
-              collider->pCollider,
-              compareCollider->pCollider,
-              transform->position,
-              compareTransform->position);
-        }
-
-        // sphere v. sphere
-        else if (collider->type == COLLIDER_SPHERE &&
-                 compareCollider->type == COLLIDER_SPHERE) {
-            points = gsk_physics_collision_find_sphere_sphere(
-              ((gsk_Collider *)collider->pCollider)->collider_data,
-              ((gsk_Collider *)compareCollider->pCollider)->collider_data,
-              transform->position,
-              compareTransform->position);
-        }
-
-        // box v. plane
-        else if (collider->type == COLLIDER_BOX &&
-                 compareCollider->type == COLLIDER_PLANE) {
-            points = gsk_physics_collision_find_box_plane(
-              ((gsk_Collider *)collider->pCollider)->collider_data,
-              ((gsk_Collider *)compareCollider->pCollider)->collider_data,
-              transform->position,
-              compareTransform->position);
-        }
-
-        // box v. sphere
-        else if (collider->type == COLLIDER_BOX &&
-                 compareCollider->type == COLLIDER_SPHERE) {
-            points = gsk_physics_collision_find_box_sphere(
-              ((gsk_Collider *)collider->pCollider)->collider_data,
-              ((gsk_Collider *)compareCollider->pCollider)->collider_data,
-              transform->position,
-              compareTransform->position);
+        if (collider->type == COLLIDER_SPHERE) { // --- Sphere Collider
+            switch (compareCollider->type) {
+            case COLLIDER_SPHERE:
+                points =
+                  gsk_physics_collision_find_sphere_sphere(__CLSN_PARAMS__);
+                break;
+            case COLLIDER_PLANE:
+                points =
+                  gsk_physics_collision_find_sphere_plane(__CLSN_PARAMS__);
+                break;
+            case COLLIDER_BOX:
+            default: break;
+            };
+        } else if (collider->type == COLLIDER_PLANE) { // --- Plane Collider
+            switch (compareCollider->type) {
+            case COLLIDER_SPHERE:
+                points =
+                  gsk_physics_collision_find_plane_sphere(__CLSN_PARAMS__);
+                break;
+            case COLLIDER_PLANE:
+            case COLLIDER_BOX:
+            default: break;
+            };
+        } else if (collider->type == COLLIDER_BOX) { // --- Box Collider
+            switch (compareCollider->type) {
+            case COLLIDER_SPHERE:
+                points = gsk_physics_collision_find_box_sphere(__CLSN_PARAMS__);
+                break;
+            case COLLIDER_PLANE:
+                points = gsk_physics_collision_find_box_plane(__CLSN_PARAMS__);
+                break;
+            case COLLIDER_BOX:
+            default: break;
+            };
         }
 
         // Collision points
         if (points.has_collision) {
 
+            points_list[points_list_next] = points;
+            if (points_list_next >= MAX_COLLISION_POINTS) {
+                LOG_CRITICAL("Max collision points exceeded");
+            }
+            points_list_next++;
+        }
+    }
+
+    for (u32 i = 0; i < points_list_next; i++) {
+        if (points_list[i].has_collision) {
             if (gsk_ecs_has(e, C_RIGIDBODY)) {
                 struct ComponentRigidbody *rigidbody =
                   gsk_ecs_get(e, C_RIGIDBODY);
@@ -193,7 +204,7 @@ fixed_update(gsk_Entity e)
                 // Create a new collision result using our points
                 // TODO: Send objects A and B
                 gsk_CollisionResult result = {
-                  .points = points,
+                  .points = points_list[i],
                 };
 
                 // Send that over to the rigidbody solver list
@@ -201,9 +212,7 @@ fixed_update(gsk_Entity e)
                                         result);
             }
 
-            collider->isColliding = points.has_collision;
-            break; // TODO: should not break.
-                   //- instead, get a collection of points
+            collider->isColliding = points_list[i].has_collision;
         }
     }
 }
