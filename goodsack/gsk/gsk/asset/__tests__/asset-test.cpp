8924018d227473b4d2e720ddcf199b05e0235dc2
gabekz <gabe@kutuzov.dev>
2024-10-03 15:15:15 -0400

    [asset] updated test suite

--- goodsack/gsk/gsk/asset/__tests__/asset-test.cpp
+++ goodsack/gsk/gsk/asset/__tests__/asset-test.cpp
@@ -1,41 +1,31 @@
 /*
- * Copyright (c) 2023, Gabriel Kutuzov
+ * Copyright (c) 2024, Gabriel Kutuzov
  * SPDX-License-Identifier: MIT
  */
 
 #include <stdlib.h>
 
 #include "asset/asset_cache.h"
-#include "core/graphics/texture/texture.h"
+#include "util/sysdefs.h"
+
+// #include "core/graphics/texture/texture.h"
 
 #include <gtest/gtest.h>
 
-TEST(AssetTestSuite, FirstTest)
+#define ASSET_NAME "gsk://textures/texture0.png"
+#define ASSET_TYPE 0
+
+TEST(AssetTestSuite, HashTest)
 {
     gsk_AssetCache cache = gsk_asset_cache_init();
 
-    gsk_asset_cache_add(&cache, 0, "gsk://textures/texture0.png");
-    EXPECT_EQ(hash_table_has(&cache.asset_table, "gsk://textures/texture0.png"),
-              1);
+    gsk_asset_cache_add(&cache, ASSET_TYPE, ASSET_NAME);
+    EXPECT_EQ(hash_table_has(&cache.asset_table, ASSET_NAME), true);
 
-    EXPECT_EQ(hash_table_get(&cache.asset_table, "gsk://textures/texture0.png"),
-              1);
+    u64 expected_handle = 1; // index reference
+    EXPECT_EQ(hash_table_get(&cache.asset_table, ASSET_NAME), expected_handle);
 
     gsk_AssetCacheState *p_state =
-      gsk_asset_cache_get(&cache, 0, "gsk://textures/texture0.png");
-    EXPECT_EQ(p_state->is_loaded, 0);
-}
-
-TEST(AssetTestSuite, QuickTextureTest)
-{
-    gsk_AssetCache cache = gsk_asset_cache_init();
-
-    gsk_Texture texture = {
-      .id = 69,
-    };
-
-    gsk_asset_cache_add(&cache, 0, "gsk://textures/texture0.png");
-
-    EXPECT_EQ(hash_table_get(&cache.asset_table, "gsk://textures/texture0.png"),
-              1);
+      gsk_asset_cache_get(&cache, ASSET_TYPE, ASSET_NAME);
+    EXPECT_EQ(p_state->is_loaded, false);
 }
